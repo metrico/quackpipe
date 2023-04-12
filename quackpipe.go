716f74a8217d37cef9a401b8c3ca0c7ac8c21402
Cluas <Cluas@live.cn>
2023-04-12 10:51:55 +0800

    feat: simplify the code

--- quackpipe.go
+++ quackpipe.go
@@ -1,21 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
 	_ "embed"
 	"encoding/json"
-	"regexp"
 	"flag"
 	"fmt"
-	"time"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"bufio"
 	"os"
+	"regexp"
 	"strings"
+	"time"
 
-	_ "github.com/marcboeker/go-duckdb"
+	_ "github.com/marcboeker/go-duckdb" // load duckdb driver
 )
 
 //go:embed play.html
@@ -23,17 +23,15 @@ var staticPlay string
 
 // params for Flags
 type CommandLineFlags struct {
-	Host *string `json:"host"`
-	Port *string `json:"port"`
-	Stdin *bool `json:"stdin"`
+	Host   *string `json:"host"`
+	Port   *string `json:"port"`
+	Stdin  *bool   `json:"stdin"`
 	Format *string `json:"format"`
 }
 
 var appFlags CommandLineFlags
 
-var (
-	db *sql.DB
-)
+var db *sql.DB
 
 func check(args ...interface{}) {
 	err := args[len(args)-1]
@@ -42,8 +40,7 @@ func check(args ...interface{}) {
 	}
 }
 
-func quack(query string, stdin bool, format string) string {
-
+func quack(query string, stdin bool, format string) (string, error) {
 	var err error
 
 	db, err = sql.Open("duckdb", "")
@@ -52,57 +49,32 @@ func quack(query string, stdin bool, format string) string {
 	}
 	defer db.Close()
 
-	if stdin != true {
+	if !stdin {
 		check(db.Exec("LOAD httpfs; LOAD json; LOAD parquet;"))
 	}
 
 	startTime := time.Now()
 	rows, err := db.Query(query)
 	if err != nil {
-		return fmt.Sprint(err.Error())
+		return "", err
 	}
 	elapsedTime := time.Since(startTime)
 
-	if (format == "JSONCompact") || (format == "JSON") {
-		jsonData, err := rowsToJSON(rows, elapsedTime)
-		if err != nil {
-			return fmt.Sprint(err.Error())
-		}
-		return string(jsonData)
-	} else if format == "CSV" {
-		csvData, err := rowsToCSV(rows, false)
-		if err != nil {
-			return fmt.Sprint(err.Error())
-		}
-		return string(csvData)
-	} else if format == "CSVWithNames" {
-		csvData, err := rowsToCSV(rows, true)
-		if err != nil {
-			return fmt.Sprint(err.Error())
-		}
-		return string(csvData)
-	} else if (format == "TSVWithNames") || (format == "TabSeparatedWithNames") {
-		tsvData, err := rowsToTSV(rows, true)
-		if err != nil {
-			return fmt.Sprint(err.Error())
-		}
-		return string(tsvData)
-	} else if (format == "TSV") || (format == "TabSeparated") {
-		tsvData, err := rowsToTSV(rows, false)
-		if err != nil {
-			return fmt.Sprint(err.Error())
-		}
-		return string(tsvData)
-	} else {
-		tsvData, err := rowsToTSV(rows, false)
-		if err != nil {
-			return fmt.Sprint(err.Error())
-		}
-		return string(tsvData)
+	switch format {
+	case "JSONCompact", "JSON":
+		return rowsToJSON(rows, elapsedTime)
+	case "CSVWithNames":
+		return rowsToCSV(rows, true)
+	case "TSVWithNames", "TabSeparatedWithNames":
+		return rowsToTSV(rows, true)
+	case "TSV", "TabSeparated":
+		return rowsToTSV(rows, false)
+	default:
+		return rowsToTSV(rows, false)
 	}
 }
 
-/* init flags */
+// initFlags initializes the command line flags
 func initFlags() {
 	appFlags.Host = flag.String("host", "0.0.0.0", "API host. Default 0.0.0.0")
 	appFlags.Port = flag.String("port", "8123", "API port. Default 8123")
@@ -111,7 +83,7 @@ func initFlags() {
 	flag.Parse()
 }
 
-/* FORMAT Extractor */
+// extractAndRemoveFormat extracts the FORMAT clause from the query and returns the query without the FORMAT clause
 func extractAndRemoveFormat(input string) (string, string) {
 	re := regexp.MustCompile(`(?i)\bFORMAT\s+(\w+)\b`)
 	match := re.FindStringSubmatch(input)
@@ -122,38 +94,41 @@ func extractAndRemoveFormat(input string) (string, string) {
 	return re.ReplaceAllString(input, ""), format
 }
 
-/* JSONCompact formatter */
-type MetaData struct {
+// Metadata is the metadata for a column
+type Metadata struct {
 	Name string `json:"name"`
 	Type string `json:"type"`
 }
 
+// Statistics is the statistics for a query
 type Statistics struct {
 	Elapsed   float64 `json:"elapsed"`
 	RowsRead  int     `json:"rows_read"`
 	BytesRead int     `json:"bytes_read"`
 }
 
+// OutputJSON is the JSON output for a query
 type OutputJSON struct {
-	Meta                   []MetaData      `json:"meta"`
+	Meta                   []Metadata      `json:"meta"`
 	Data                   [][]interface{} `json:"data"`
 	Rows                   int             `json:"rows"`
 	RowsBeforeLimitAtLeast int             `json:"rows_before_limit_at_least"`
 	Statistics             Statistics      `json:"statistics"`
 }
 
-func rowsToJSON(rows *sql.Rows, elapsedTime time.Duration) ([]byte, error) {
+// rowsToJSON converts the rows to JSON string
+func rowsToJSON(rows *sql.Rows, elapsedTime time.Duration) (string, error) {
 	defer rows.Close()
 
 	// Get column names
 	columns, err := rows.Columns()
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	// Create a slice to store maps of column names and their corresponding values
 	var results OutputJSON
-	results.Meta = make([]MetaData, len(columns))
+	results.Meta = make([]Metadata, len(columns))
 	results.Data = make([][]interface{}, 0)
 
 	for i, column := range columns {
@@ -170,7 +145,7 @@ func rowsToJSON(rows *sql.Rows, elapsedTime time.Duration) ([]byte, error) {
 		// Scan the values from the row into the pointers
 		err := rows.Scan(values...)
 		if err != nil {
-			return nil, err
+			return "", err
 		}
 
 		// Create a slice to hold the row data
@@ -189,28 +164,26 @@ func rowsToJSON(rows *sql.Rows, elapsedTime time.Duration) ([]byte, error) {
 
 	err = rows.Err()
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	results.Rows = len(results.Data)
 	results.RowsBeforeLimitAtLeast = len(results.Data)
-
 
 	// Populate the statistics object with number of rows, bytes, and elapsed time
 	results.Statistics.Elapsed = elapsedTime.Seconds()
 	results.Statistics.RowsRead = results.Rows
 	// Note: bytes_read is an approximation, it's just the number of rows * number of columns
 	// results.Statistics.BytesRead = results.Rows * len(columns) * 8 // Assuming each value takes 8 bytes
-
 	jsonData, err := json.Marshal(results)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	return jsonData, nil
+	return string(jsonData), nil
 }
 
-/* TSV formatter */
+// rowsToTSV converts the rows to TSV string
 func rowsToTSV(rows *sql.Rows, cols bool) (string, error) {
 	var result []string
 	columns, err := rows.Columns()
@@ -218,11 +191,11 @@ func rowsToTSV(rows *sql.Rows, cols bool) (string, error) {
 		return "", err
 	}
 
-	if cols == true {
+	if cols {
 		// Append column names as the first row
 		result = append(result, strings.Join(columns, "\t"))
 	}
-	
+
 	// Fetch rows and append their values as tab-delimited lines
 	values := make([]interface{}, len(columns))
 	scanArgs := make([]interface{}, len(columns))
@@ -249,7 +222,7 @@ func rowsToTSV(rows *sql.Rows, cols bool) (string, error) {
 	return strings.Join(result, "\n"), nil
 }
 
-/* CSV Formatter */
+// rowsToCSV converts the rows to CSV string
 func rowsToCSV(rows *sql.Rows, cols bool) (string, error) {
 	var result []string
 	columns, err := rows.Columns()
@@ -257,11 +230,11 @@ func rowsToCSV(rows *sql.Rows, cols bool) (string, error) {
 		return "", err
 	}
 
-	if cols == true {
+	if cols {
 		// Append column names as the first row
 		result = append(result, strings.Join(columns, ","))
 	}
-	
+
 	// Fetch rows and append their values as CSV rows
 	values := make([]interface{}, len(columns))
 	scanArgs := make([]interface{}, len(columns))
@@ -288,18 +261,12 @@ func rowsToCSV(rows *sql.Rows, cols bool) (string, error) {
 	return strings.Join(result, "\n"), nil
 }
 
-
-
-/* main */
-
 func main() {
-
-   initFlags()
-   if *appFlags.Stdin == true {
-
+	initFlags()
+	if *appFlags.Stdin {
 		scanner := bufio.NewScanner((os.Stdin))
 		query := ""
-	        default_format := *appFlags.Format
+		default_format := *appFlags.Format
 		for scanner.Scan() {
 			query = query + "\n" + scanner.Text()
 		}
@@ -311,66 +278,71 @@ func main() {
 			query = cleanquery
 			default_format = format
 		}
-		result := quack(query, true, default_format)
-		fmt.Println(result)
-
-   } else {	
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		var bodyBytes []byte
-		var query string
-		var err error
-
-		/* Query Handler */
-		if r.URL.Query().Get("query") != "" {
-			query = r.Form.Get("query")
-		} else if r.Body != nil {
-			bodyBytes, err = ioutil.ReadAll(r.Body)
-			if err != nil {
-				fmt.Printf("Body reading error: %v", err)
-				return
-			}
-			defer r.Body.Close()
-			query = string(bodyBytes)
-		}
-
-		switch r.Header.Get("Accept") {
-		case "application/json":
-			w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		case "application/xml":
-			w.Header().Set("Content-Type", "application/xml; charset=utf-8")
-		case "text/css":
-			w.Header().Set("Content-Type", "text/css; charset=utf-8")
-		default:
-			w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		}
-		
-		/* Format Handler */
-		default_format := *appFlags.Format
-		if r.URL.Query().Get("default_format") != "" {
-			default_format = r.URL.Query().Get("default_format")
-		}
-		
-		/* Extract FORMAT from query and override the current `default_format` */
-		cleanquery, format := extractAndRemoveFormat(query)
-		if len(format) > 0 {
-			query = cleanquery
-			default_format = format
-		}
-
-		if len(query) == 0 {
-			w.Write([]byte(staticPlay))
+		result, err := quack(query, true, default_format)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
 		} else {
-			result := quack(query, false, default_format)
-			w.Write([]byte(result))
+			fmt.Println(result)
 		}
-	})
+	} else {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			var bodyBytes []byte
+			var query string
+			var err error
 
-	fmt.Printf("API Running: %s:%s\n", *appFlags.Host, *appFlags.Port)
-	if err := http.ListenAndServe(*appFlags.Host+":"+*appFlags.Port, nil); err != nil {
-		panic(err)
+			// handle query parameter
+			if r.URL.Query().Get("query") != "" {
+				query = r.Form.Get("query")
+			} else if r.Body != nil {
+				bodyBytes, err = ioutil.ReadAll(r.Body)
+				if err != nil {
+					fmt.Printf("Body reading error: %v", err)
+					return
+				}
+				defer r.Body.Close()
+				query = string(bodyBytes)
+			}
+
+			switch r.Header.Get("Accept") {
+			case "application/json":
+				w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			case "application/xml":
+				w.Header().Set("Content-Type", "application/xml; charset=utf-8")
+			case "text/css":
+				w.Header().Set("Content-Type", "text/css; charset=utf-8")
+			default:
+				w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			}
+
+			// format handling
+			default_format := *appFlags.Format
+			if r.URL.Query().Get("default_format") != "" {
+				default_format = r.URL.Query().Get("default_format")
+			}
+
+			// extract FORMAT from query and override the current `default_format`
+			cleanquery, format := extractAndRemoveFormat(query)
+			if len(format) > 0 {
+				query = cleanquery
+				default_format = format
+			}
+
+			if len(query) == 0 {
+				_, _ = w.Write([]byte(staticPlay))
+			} else {
+				result, err := quack(query, false, default_format)
+				if err != nil {
+					_, _ = w.Write([]byte(err.Error()))
+				} else {
+					_, _ = w.Write([]byte(result))
+				}
+			}
+		})
+
+		fmt.Printf("API Running: %s:%s\n", *appFlags.Host, *appFlags.Port)
+		if err := http.ListenAndServe(*appFlags.Host+":"+*appFlags.Port, nil); err != nil {
+			panic(err)
+		}
 	}
-
-   }
 }
