d382b3be4de40d69ac8e66670b3a3f521d058a5b
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2024-09-25 10:43:52 +0200

    No aliases in stdin mode

--- service/db/db.go
+++ service/db/db.go
@@ -28,10 +28,9 @@ func Quack(appFlags model.CommandLineFlags, query string, stdin bool, params str
 		check(db.ExecContext(context.Background(), "LOAD httpfs; LOAD json; LOAD parquet;"))
 		check(db.ExecContext(context.Background(), "SET autoinstall_known_extensions=1;"))
 		check(db.ExecContext(context.Background(), "SET autoload_known_extensions=1;"))
-	}
-
-	if alias {
-		check(db.ExecContext(context.Background(), "LOAD chsql;"))
+		if alias {
+			check(db.ExecContext(context.Background(), "LOAD chsql;"))
+		}
 	}
 
 	if (md) && (motherduck != "") {
