0bea03c579333ff9ad73377607ba82ba1e3558be
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2025-01-02 18:15:43 +0100

    LOAD chsql_native in alias mode

--- service/db/db.go
+++ service/db/db.go
@@ -29,7 +29,7 @@ func Quack(appFlags model.CommandLineFlags, query string, stdin bool, params str
 		check(db.ExecContext(context.Background(), "SET autoinstall_known_extensions=1;"))
 		check(db.ExecContext(context.Background(), "SET autoload_known_extensions=1;"))
 		if alias {
-			check(db.ExecContext(context.Background(), "LOAD chsql;"))
+			check(db.ExecContext(context.Background(), "LOAD chsql; LOAD chsql_native;"))
 		}
 	}
 
