a926bcf74788269219666686ea62ef5b087d8277
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2025-04-21 21:01:36 +0200

    204 to Inserts
    Respond with 204 to Insert requests

--- merge/handlers/insert_into.go
+++ merge/handlers/insert_into.go
@@ -60,7 +60,7 @@ func InsertIntoHandler(w http.ResponseWriter, r *http.Request) error {
 			return err
 		}
 	}
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Ok"))
+	w.WriteHeader(http.StatusNoContent)
+	// w.Write([]byte("Ok"))
 	return nil
 }
